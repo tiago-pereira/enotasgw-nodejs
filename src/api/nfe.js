cab300509f8b6ab45531a6fcecb2c5583da9b21a
Wellington Silverio <wellington.silverio@caeplab.com.br>
2019-01-16 16:46:00 -0200

    <fix>: filtro

--- src/api/nfe.js
+++ src/api/nfe.js
@@ -44,7 +44,7 @@ module.exports = class NFE {
     dataFinal = moment(dataFinal).utc().format();
     console.log(dataInicial, dataFinal);
     
-    const filtro = `dataCriacao ge ${dataInicial} and dataCriacao le ${dataFinal}`;
+    const filtro = `dataCriacao ge '${dataInicial}' and dataCriacao le '${dataFinal}'`;
 
     return this.client.get(`${PREFIX}/${idEmpresa}/nfes?pageNumber=${numeroPagina}&pageSize=${tamanhoPagina}&filter=${filtro}`);
   }
